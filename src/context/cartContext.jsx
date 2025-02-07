e8252045e6c6ff99950ad0a94aca2da2453fd60c
Leandro Insfran <leandro.insfran@systelec.com>
2025-02-07 17:35:48 -0300

    ..

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext();
 
-const carritoInicial = JSON.parse(localStorage.getItem("carrito") || [])
+const carritoInicial = JSON.parse(localStorage.getItem("carrito") || "[]")
 
 export const CartProvider = ({children}) => {
 
